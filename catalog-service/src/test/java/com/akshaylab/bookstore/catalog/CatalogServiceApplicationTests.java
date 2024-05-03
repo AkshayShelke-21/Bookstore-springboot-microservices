a2079a76a709a22528c62df406199156462c0a03
AkshayShelke21 <akshelke21@gmail.com>
2024-05-03 16:38:17 +0530

    Spring Doc, Rest Assured, SpotlessM Done.

--- catalog-service/src/test/java/com/akshaylab/bookstore/catalog/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/akshaylab/bookstore/catalog/CatalogServiceApplicationTests.java
@@ -2,12 +2,14 @@ package com.akshaylab.bookstore.catalog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(
+        ContainersConfig.class
+)
 class CatalogServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
