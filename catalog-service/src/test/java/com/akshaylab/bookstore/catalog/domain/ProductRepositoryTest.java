298a5ee513f11505311731ee94b88505b0b58a2c
AkshayShelke21 <akshelke21@gmail.com>
2024-05-04 14:22:06 +0530

    Products API and Tests Done [2].

--- catalog-service/src/test/java/com/akshaylab/bookstore/catalog/domain/ProductRepositoryTest.java
+++ catalog-service/src/test/java/com/akshaylab/bookstore/catalog/domain/ProductRepositoryTest.java
@@ -1,14 +1,13 @@
 package com.akshaylab.bookstore.catalog.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
-
-import java.math.BigDecimal;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest(
         properties = {
