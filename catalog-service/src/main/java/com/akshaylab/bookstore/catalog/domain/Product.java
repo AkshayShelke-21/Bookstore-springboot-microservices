fb5014a85750f2386f982e3f957fd6476ecfcf50
AkshayShelke21 <akshelke21@gmail.com>
2024-05-04 10:17:42 +0530

    BD Migration and Product APIs Done.

---
+++ catalog-service/src/main/java/com/akshaylab/bookstore/catalog/domain/Product.java
@@ -0,0 +1,8 @@
+package com.akshaylab.bookstore.catalog.domain;
+
+import java.math.BigDecimal;
+public record Product(String code,
+                      String name,
+                      String description,
+                      String imageUrl,
+                      BigDecimal price) {}
---
+++ catalog-service/src/main/java/com/akshaylab/bookstore/catalog/web/Controllers/ProductController.java
@@ -0,0 +1,26 @@
+package com.akshaylab.bookstore.catalog.web.Controllers;
+
+import com.akshaylab.bookstore.catalog.domain.PagedResult;
+import com.akshaylab.bookstore.catalog.domain.Product;
+import com.akshaylab.bookstore.catalog.domain.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    PagedResult<Product> getProducts(@RequestParam(name = "page", defaultValue = "1") int pageNo){
+        return productService.getProducts(pageNo);
+    }
+}
